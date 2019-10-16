514da486a2ebcf3fbdb92732503b172148389257
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2019-10-16 23:25:30 +0300

    Initial Commit

---
+++ MatchingCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatchingCards
+//
+//  Created by Yurii Serediuk on 16.10.2019.
+//  Copyright © 2019 Yurii Serediuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
